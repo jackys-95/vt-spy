73642a42fc380c87cc98df3c3c4f74b793536c3d
Jacky Shum <j_shum55@outlook.com>
2022-08-29 23:45:02 -0700

    Added regex for NYSE and NASDAQ ticker symbols.

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
     "fmt"
     "flag"
+    "regexp"
 )
 
 const WatchlistFlagName = "watchlist"
 const TickerFlagName = "ticker"
+const NYSEregex = `[a-zA-Z]{4}`
+const NASDAQregex = `[a-zA-Z]{4}`
 
 var watchListFlag *bool
 var tickerFlag *string
@@ -16,12 +19,21 @@ func init() {
     tickerFlag = flag.String(TickerFlagName, "", "Symbol for the stock ticker you wish to query.")
 }
 
+func validateTickerSymbol(symbol string) bool {
+    nyseRe, nasdaqRe := regexp.MustCompile(NYSEregex), regexp.MustCompile(NASDAQregex)
+    return nyseRe.MatchString(symbol) || nasdaqRe.MatchString(symbol)
+}
+
+func validateTickers() {
+}
+
 func main() {
     flag.Parse()
     fmt.Println("Watchlist has value:", *watchListFlag)
     fmt.Println("Ticker has value:", *tickerFlag)
     ticker := NewTicker(*tickerFlag, "NASDAQ", "USD")
     fmt.Println(*ticker)
+    fmt.Println(validateTickerSymbol(ticker.Symbol))
     WriteTickerToJsonFile(ticker)
 }
 
